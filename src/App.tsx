84cc1e2519649beb9877b872a2f41f98804d8536
sobadrush <sobadrush@icloud.com>
2020-11-08 22:18:16 +0800

    將function Component改成class

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PropsType {
+  
 }
 
-export default App;
+interface StateType {
+  myDate: Date
+}
+
+export default class App extends React.Component<PropsType, StateType> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = { myDate: new Date() };
+  }
+
+  render() {
+    console.log("App >>> this.state.myDate >>> ", this.state.myDate);
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
