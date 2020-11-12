d12f1dadc36320b4c6e4f6b8391b8dd0b50fb2a8
sobadrush <sobadrush@icloud.com>
2020-11-12 23:18:11 +0800

    React.Fragment

--- src/components/MyContent/MyContentComponent.tsx
+++ src/components/MyContent/MyContentComponent.tsx
@@ -19,36 +19,39 @@ export default class MyContentComponent extends React.Component<any, any> {
 
     render(): any {
         return (
-            <div>
-                {/* <h1>我是MyContentComponent</h1> */}
+            // React 16.2 開始提供了 Fragment (片段) 功能，Fragment 讓你可以直接返回一組元素，而不需要增加多餘的 DOM 節點
+            // 語法糖: <></>
+            <React.Fragment>
+                <div>
+                    {/* <h1>我是MyContentComponent</h1> */}
 
-                {/* ※ 改完路由要進行重起才會生效 */}
-                {/* 不使用<Switch>包裹的情况下，每一个被location匹配到的<Route>将都会被渲染 */}
-                <Switch>
+                    {/* ※ 改完路由要進行重起才會生效 */}
+                    {/* 不使用<Switch>包裹的情况下，每一个被location匹配到的<Route>将都会被渲染 */}
+                    <Switch>
 
-                    {/* exact: 是否精準匹配, 若此處不加exact, 則帶參數的URL會被此路由攔截到, 便無法取得參數 */}
-                    <Redirect from='/' to='/homePage' exact={true}/>
-                    <Route path="/homePage" component={MyHomePageComponent}/>
+                        {/* exact: 是否精準匹配, 若此處不加exact, 則帶參數的URL會被此路由攔截到, 便無法取得參數 */}
+                        <Redirect from='/' to='/homePage' exact={true}/>
+                        <Route path="/homePage" component={MyHomePageComponent}/>
 
-                    <Route path="/poem1" component={MyPoemComponentA}/>
-                    <Route path="/poem2" component={MyPoemComponentB}/>
-                    <Route path="/poem3" component={MyPoemComponentC}/>
+                        <Route path="/poem1" component={MyPoemComponentA}/>
+                        <Route path="/poem2" component={MyPoemComponentB}/>
+                        <Route path="/poem3" component={MyPoemComponentC}/>
 
-                    {/* 路由參數 */}
-                    <Route path="/songs/:id" component={MySongComponent}/>
+                        {/* 路由參數 */}
+                        <Route path="/songs/:id" component={MySongComponent}/>
 
-                    <Route path="/shopManagement" component={MyShopComponent}/>
+                        <Route path="/shopManagement" component={MyShopComponent}/>
 
-                    {/* Redirect: 路由重定向：將 /GG919，Redirect 到 /poem2 */}
-                    <Redirect from='/GG919' to='/poem2' />
-                    
-                    {/* 萬用路由 1: 轉導到 /homePage */}
-                    {/* <Redirect from='*' to='/homePage' /> */}
-                    {/* 萬用路由 2: 直接開 NoMatch 元件 */}
-                    <Route path="*" component={NoMatch} />
-                </Switch>
-            </div>
-
+                        {/* Redirect: 路由重定向：將 /GG919，Redirect 到 /poem2 */}
+                        <Redirect from='/GG919' to='/poem2' />
+                        
+                        {/* 萬用路由 1: 轉導到 /homePage */}
+                        {/* <Redirect from='*' to='/homePage' /> */}
+                        {/* 萬用路由 2: 直接開 NoMatch 元件 */}
+                        <Route path="*" component={NoMatch} />
+                    </Switch>
+                </div>
+            </React.Fragment>
         );
     }
 }
